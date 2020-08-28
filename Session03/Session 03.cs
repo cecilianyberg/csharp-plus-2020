cb8226fdf5f675617a17ef0e61b518bf069e5782
ELLIESDATOR\cicci <cicci81@hotmail.com>
2020-08-28 13:24:27 +0200

    In progress

---
+++ Session03/Session 03.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Session02Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange ett antal siffror, separerat med kommatecken.");
+            var input = Console.ReadLine();
+            var inputArray = input.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            
+            foreach (var number in inputArray)
+            {
+                Console.WriteLine("Värdet är " + number);
+            }
+        }
+    }
+}
---
+++ Session03/Session03/Session03Excercise02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Exercise03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange ett antal siffror, separerat med kommatecken.");
+            string input = Console.ReadLine();
+            string[] inputArray = input.Split(",", StringSplitOptions.RemoveEmptyEntries);
+            double?[] numberArray = new double?[inputArray.Length];
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                try
+                {
+                    numberArray[i] = Convert.ToDouble(inputArray[i]);
+                }
+                catch (Exception)
+                {
+                    numberArray[i] = null;
+                }
+            }
+
+            //Console.WriteLine("Värdet är " + number);
+
+        }
+    }
+}
